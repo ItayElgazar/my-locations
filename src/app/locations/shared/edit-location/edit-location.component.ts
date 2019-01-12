95472d1157c49b2867a76ae4ae5da4d0e05ad835
itayelgazar <itayekk1@gmail.com>
2019-01-12 16:50:28 +0100

    Added edit-location component

---
+++ src/app/locations/shared/edit-location/edit-location.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-location',
+  templateUrl: './edit-location.component.html',
+  styleUrls: ['./edit-location.component.scss']
+})
+export class EditLocationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
