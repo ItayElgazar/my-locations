304bafb140936311aa7c84846b6754fb5b068abd
itayelgazar <itayekk1@gmail.com>
2019-01-12 23:44:48 +0100

    Added add location component

--- src/app/locations/add-location/add-location.component.ts
+++ src/app/locations/add-location/add-location.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {LocationsService} from '../shared/locations.service';
+import { Location } from '../shared/location';
+import {Observable} from 'rxjs';
+import {Category} from '../../categories/shared/category';
+import {CategoriesService} from '../../categories/shared/categories.service';
 
 @Component({
   selector: 'app-add-location',
@@ -7,9 +14,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddLocationComponent implements OnInit {
 
-  constructor() { }
+  private locationForm: FormGroup;
+  categories$: Observable<Category[]>;
+  constructor(private formBuilder: FormBuilder,
+              private locationsService: LocationsService,
+              private categoriesService: CategoriesService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.categories$ =  this.categoriesService.getCategories();
+    // I could add more custom validators (for example for the coordinates) but for the sake of simplicity, i'll not do that.
+    this.locationForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      address: ['', Validators.required],
+      coordinates: ['', Validators.required],
+      category: ['', Validators.required]
+    });
+  }
+
+  addLocation() {
+    const category: Category = this.categoriesService.getCategoryByName(this.locationForm.controls.category.value);
+    if (this.locationForm.invalid || !category) {
+      return;
+    }
+    const location: Location = this.locationsService.buildLocation({...this.locationForm.value, category});
+    this.locationsService.addLocation(location);
+    this.router.navigate(['locations']);
   }
 
 }
